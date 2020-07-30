5807720b4844fe136af8c4b5a28ee040b533d0ea
hakkeray <rukeine@gmail.com>
2020-07-30 00:18:47 +0000

    revised query

--- embulk_to_mysql/queries/create_customers.sql
+++ embulk_to_mysql/queries/create_customers.sql
@@ -1,4 +1,14 @@
 --create_customers.sql--
+WITH t AS (SELECT user_id, MAX(timestamp) as time 
+FROM pageviews_tmp 
+GROUP BY user_id)
+, s AS (SELECT p.user_id, p.user_agent, p.timestamp 
+  FROM pageviews_tmp p 
+  JOIN t ON p.user_id = t.user_id 
+  AND p.timestamp = t.time) 
+SELECT c.user_id, c.first_name, c.last_name, c.job_title, s.user_agent AS operating_system 
+FROM customers_tmp c 
+JOIN s ON c.user_id = s.user_id
 CREATE TABLE customers
 SELECT c.user_id, c.first_name, c.last_name, c.job_title, p.user_agent AS operating_system 
 FROM pageviews_tmp p
